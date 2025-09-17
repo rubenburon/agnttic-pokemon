aaf744af7d9499c5e5f365006b7e4d1b7a7136b3
Rubén Burón <rubenbro@ext.inditex.com>
2025-09-17 13:32:45 +0200

    feat: use .env GEMINI_API_KEY

--- mcp/tools.py
+++ mcp/tools.py
@@ -1,8 +1,12 @@
+import os
+from dotenv import load_dotenv
 import google.generativeai as genai
+
+load_dotenv()
 
 class GeminiTool:
     def __init__(self, api_key=None, model="gemini-2.5-flash"):
-        self.api_key = api_key or "AIzaSyALmQ9eqDaiiaj0YOCmHbVDvc6QdFUnUJY"
+        self.api_key = api_key or os.getenv("GEMINI_API_KEY")
         self.model = model
         genai.configure(api_key=self.api_key)
 
